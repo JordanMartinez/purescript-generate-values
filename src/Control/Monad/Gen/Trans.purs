60a9310e482376e1ad5a15aa7d8a5fd60041f81c
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-03-29 08:24:31 -0500

    Make code compile

--- src/Control/Monad/Gen/Trans.purs
+++ src/Control/Monad/Gen/Trans.purs
@@ -1,11 +1,24 @@
 -- | This module defines the random generator monad used by the `Test.QuickCheck`
 -- | module, as well as helper functions for constructing random generators.
-module Test.QuickCheck.Gen
-  ( Gen
-  , unGen
+module Control.Monad.Gen.Trans
+  ( Size
   , GenState
-  , Size
-  , repeatable
+  , GenT
+  , runGenT
+  , runGenT'
+  , withGenT
+  , mapGenT
+  , execGenT
+  , evalGenT
+  , perturbGenT
+  , Gen
+  , runGen
+  , withGen
+  , mapGen
+  , execGen
+  , evalGen
+  , perturbGen
+  -- , repeatable
   , stateful
   , variant
   , suchThat
@@ -22,9 +35,6 @@ module Test.QuickCheck.Gen
   , vectorOf
   , elements
   , shuffle
-  , runGen
-  , evalGen
-  , perturbGen
   , uniform
   , sample
   , randomSample
@@ -38,21 +48,23 @@ import Control.Alt (class Alt)
 import Control.Lazy (class Lazy)
 import Control.Monad.Gen.Class (class MonadGen)
 import Control.Monad.Rec.Class (class MonadRec, Step(..), tailRecM)
-import Control.Monad.State (State, runState, evalState)
-import Control.Monad.State.Class (modify, state)
+import Control.Monad.State (evalState, execState, mapState, modify_, runState, withState)
+import Control.Monad.State.Class (state)
+import Control.Monad.State.Trans (StateT(..), evalStateT, execStateT, mapStateT, runStateT, withStateT)
 import Data.Array ((:), length, zip, sortBy)
 import Data.Array.NonEmpty (NonEmptyArray)
 import Data.Array.NonEmpty as NEA
 import Data.Enum (class BoundedEnum, fromEnum, toEnum)
-import Data.Semigroup.Foldable (foldMap1)
+import Data.Identity (Identity)
 import Data.Int (toNumber, floor)
 import Data.List (List(..), toUnfoldable)
 import Data.Maybe (Maybe(..), fromJust)
 import Data.Monoid.Additive (Additive(..))
 import Data.Newtype (unwrap)
 import Data.Number ((%))
+import Data.Semigroup.Foldable (foldMap1)
 import Data.Tuple (Tuple(..), fst, snd)
-import Effect (Effect)
+import Effect.Class (class MonadEffect, liftEffect)
 import Partial.Unsafe (unsafePartial)
 import Random.LCG (Seed, lcgPerturb, lcgM, lcgNext, unSeed, randomSeed)
 
@@ -65,64 +77,113 @@ type GenState = { newSeed :: Seed, size :: Size }
 
 -- | The random generator monad
 -- |
--- | `Gen` is a state monad which encodes a linear congruential generator.
-newtype Gen a = Gen (State GenState a)
+-- | `GenT` is a state monad which encodes a linear congruential generator.
+newtype GenT m a = GenT (StateT GenState m a)
 
-derive newtype instance functorGen :: Functor Gen
-derive newtype instance applyGen :: Apply Gen
-derive newtype instance applicativeGen :: Applicative Gen
-derive newtype instance bindGen :: Bind Gen
-derive newtype instance monadGen :: Monad Gen
-derive newtype instance altGen :: Alt Gen
-derive newtype instance monadRecGen :: MonadRec Gen
-derive newtype instance lazyGen :: Lazy (Gen a)
+derive newtype instance Functor m => Functor (GenT m)
+derive newtype instance Monad m => Apply (GenT m)
+derive newtype instance Monad m => Applicative (GenT m)
+derive newtype instance Monad m => Bind (GenT m)
+derive newtype instance Monad m => Monad (GenT m)
+derive newtype instance (Monad m, Alt m) => Alt (GenT m)
+derive newtype instance MonadRec m => MonadRec (GenT m)
+derive newtype instance Lazy (m a) => Lazy (GenT m a)
 
-instance monadGenGen :: MonadGen Gen where
+instance Monad m => MonadGen (GenT m) where
   chooseInt = chooseInt
   chooseFloat = choose
   chooseBool = (_ < 0.5) <$> uniform
   resize f g = sized \s -> resize (f s) g
   sized = sized
 
--- | Exposes the underlying State implementation.
-unGen :: forall a. Gen a -> State GenState a
-unGen (Gen st) = st
+-- | Exposes the underlying function.
+runGenT :: forall m a. GenT m a -> GenState -> m (Tuple a GenState)
+runGenT gen s = runStateT (runGenT' gen) s
+
+-- | Exposes the underlying StateT implementation.
+runGenT' :: forall m a. GenT m a -> StateT GenState m a
+runGenT' (GenT st) = st
+
+-- | Run a random generator, keeping only the randomly-generated result
+withGenT :: forall m a. (GenState -> GenState) -> GenT m a -> GenT m a
+withGenT f = GenT <<< withStateT f <<< runGenT'
+
+mapGenT :: forall m1 m2 a b. (m1 (Tuple a GenState) -> m2 (Tuple b GenState)) -> GenT m1 a -> GenT m2 b
+mapGenT f = GenT <<< mapStateT f <<< runGenT'
+
+execGenT :: forall m a. Functor m => GenT m a -> GenState -> m GenState
+execGenT = execStateT <<< runGenT'
+
+-- | Run a random generator, keeping only the randomly-generated result
+evalGenT :: forall m a. Functor m => GenT m a -> GenState -> m a
+evalGenT = evalStateT <<< runGenT'
+
+-- | Perturb a random generator by modifying the current seed
+perturbGenT :: forall m a. Monad m => Number -> GenT m a -> GenT m a
+perturbGenT n gen = GenT do
+  modify_ \s -> s { newSeed = lcgPerturb (float32ToInt32 n) s.newSeed }
+  runGenT' gen
+
+type Gen a = GenT Identity a
+
+-- | Run a random generator
+runGen :: forall a. Gen a -> GenState -> Tuple a GenState
+runGen = runState <<< runGenT'
+
+-- | Run a random generator, keeping only the randomly-generated result
+withGen :: forall a. (GenState -> GenState) -> Gen a -> Gen a
+withGen f = GenT <<< withState f <<< runGenT'
+
+mapGen :: forall a b. (Tuple a GenState -> Tuple b GenState) -> Gen a -> Gen b
+mapGen f = GenT <<< mapState f <<< runGenT'
+
+execGen :: forall a. Gen a -> GenState -> GenState
+execGen = execState <<< runGenT'
+
+-- | Run a random generator, keeping only the randomly-generated result
+evalGen :: forall a. Gen a -> GenState -> a
+evalGen = evalState <<< runGenT'
+
+-- | Perturb a random generator by modifying the current seed
+perturbGen :: forall m a. Monad m => Number -> Gen a -> Gen a
+perturbGen = perturbGenT
 
 -- | Create a random generator for a function type.
-repeatable :: forall a b. (a -> Gen b) -> Gen (a -> b)
-repeatable f = Gen $ state \s -> Tuple (\a -> fst (runGen (f a) s)) (s { newSeed = lcgNext s.newSeed })
+-- repeatable :: forall m a b. (a -> GenT m b) -> GenT m (a -> b)
+-- repeatable f = GenT $ state \s -> Tuple (\a -> fst (runGenT (f a) s)) (s { newSeed = lcgNext s.newSeed })
 
 -- | Create a random generator which uses the generator state explicitly.
-stateful :: forall a. (GenState -> Gen a) -> Gen a
-stateful f = Gen $ state \s -> runGen (f s) s
+stateful :: forall m a. (GenState -> GenT m a) -> GenT m a
+stateful f = GenT $ StateT \s -> runGenT (f s) s
 
 -- | Modify a random generator by setting a new random seed.
-variant :: forall a. Seed -> Gen a -> Gen a
-variant n g = Gen $ state \s -> runGen g s { newSeed = n }
+variant :: forall m a. Seed -> GenT m a -> GenT m a
+variant n g = GenT $ StateT \s -> runGenT g s { newSeed = n }
 
 -- | Ensure that a generator only produces values that match a predicate. If
 -- | the predicate always returns false the generator will loop forever.
-suchThat :: forall a. Gen a -> (a -> Boolean) -> Gen a
+suchThat :: forall m a. MonadRec m => GenT m a -> (a -> Boolean) -> GenT m a
 suchThat gen pred = tailRecM go unit
   where
-  go :: Unit -> Gen (Step Unit a)
+  go :: Unit -> GenT m (Step Unit a)
   go _ = do
     a <- gen
     pure if pred a then Done a else Loop unit
 
 -- | Create a random generator which depends on the size parameter.
-sized :: forall a. (Size -> Gen a) -> Gen a
+sized :: forall m a. (Size -> GenT m a) -> GenT m a
 sized f = stateful (\s -> f s.size)
 
 -- | Modify a random generator by setting a new size parameter.
-resize :: forall a. Size -> Gen a -> Gen a
-resize sz g = Gen $ state \{ newSeed, size } ->
-  (_ {size = size} ) <$> runGen g { newSeed, size: sz}
+resize :: forall m a. Monad m => Size -> GenT m a -> GenT m a
+resize sz g = GenT $ StateT \{ newSeed, size } ->
+  (map _ {size = size} ) <$> runGenT g { newSeed, size: sz}
 
 -- | Create a random generator which samples a range of `Number`s i
 -- | with uniform probability.
-choose :: Number -> Number -> Gen Number
-choose a b = (*) (max' - min') >>> (+) min' >>> unscale <$> uniform where
+choose :: forall m. Monad m => Number -> Number -> GenT m Number
+choose a b = (*) (max' - min') >>> (+) min' >>> unscale <$> uniform
+  where
   unscale = (_ * 2.0)
   scale = (_ * 0.5)
   min' = scale $ min a b
@@ -131,18 +192,18 @@ choose a b = (*) (max' - min') >>> (+) min' >>> unscale <$> uniform where
 -- | Create a random generator which chooses uniformly distributed
 -- | integers from the closed interval `[a, b]`.
 -- | Note that very large intervals will cause a loss of uniformity.
-chooseInt :: Int -> Int -> Gen Int
+chooseInt :: forall m. Monad m => Int -> Int -> GenT m Int
 chooseInt a b = if a <= b then chooseInt' a b else chooseInt' b a
 
 -- guaranteed a <= b
-chooseInt' :: Int -> Int -> Gen Int
+chooseInt' :: forall m. Monad m => Int -> Int -> GenT m Int
 chooseInt' a b = floor <<< clamp <$> choose32BitPosNumber
   where
-    choose32BitPosNumber :: Gen Number
+    choose32BitPosNumber :: GenT m Number
     choose32BitPosNumber =
       (+) <$> choose31BitPosNumber <*> (((*) 2.0) <$> choose31BitPosNumber)
 
-    choose31BitPosNumber :: Gen Number
+    choose31BitPosNumber :: GenT m Number
     choose31BitPosNumber = toNumber <$> lcgStep
 
     clamp :: Number -> Number
@@ -153,14 +214,14 @@ chooseInt' a b = floor <<< clamp <$> choose32BitPosNumber
 
 -- | Create a random generator which selects and executes a random generator from
 -- | a non-empty array of random generators with uniform probability.
-oneOf :: forall a. NonEmptyArray (Gen a) -> Gen a
+oneOf :: forall m a. Monad m => NonEmptyArray (GenT m a) -> GenT m a
 oneOf xs = do
   n <- chooseInt zero (NEA.length xs - one)
   unsafePartial $ NEA.unsafeIndex xs n
 
 -- | Create a random generator which selects and executes a random generator from
 -- | a non-empty, weighted list of random generators.
-frequency :: forall a. NonEmptyArray (Tuple Number (Gen a)) -> Gen a
+frequency :: forall m a. Monad m => NonEmptyArray (Tuple Number (GenT m a)) -> GenT m a
 frequency xxs =
   let
     default = snd $ NEA.head xxs
@@ -175,13 +236,13 @@ frequency xxs =
     pick 0 n
 
 -- | Create a random generator which generates an array of random values.
-arrayOf :: forall a. Gen a -> Gen (Array a)
+arrayOf :: forall m a. MonadRec m => GenT m a -> GenT m (Array a)
 arrayOf g = sized $ \n ->
   do k <- chooseInt zero n
      vectorOf k g
 
 -- | Create a random generator which generates a non-empty array of random values.
-arrayOf1 :: forall a. Gen a -> Gen (NonEmptyArray a)
+arrayOf1 :: forall m a. MonadRec m => GenT m a -> GenT m (NonEmptyArray a)
 arrayOf1 g = sized $ \n ->
   do k <- chooseInt zero n
      x <- g
@@ -192,7 +253,7 @@ arrayOf1 g = sized $ \n ->
 -- | `toEnum i` must be well-behaved:
 -- | It must return a value wrapped in Just for all Ints between
 -- | `fromEnum bottom` and `fromEnum top`.
-enum :: forall a. BoundedEnum a => Gen a
+enum :: forall m a. Monad m => BoundedEnum a => GenT m a
 enum = do
   i <- chooseInt (fromEnum (bottom :: a)) (fromEnum (top :: a))
   pure (unsafePartial $ fromJust $ toEnum i)
@@ -206,67 +267,53 @@ replicateMRec k gen = tailRecM go (Tuple Nil k)
   go (Tuple acc n) = gen <#> \x -> Loop (Tuple (Cons x acc) (n - 1))
 
 -- | Create a random generator which generates a list of random values of the specified size.
-listOf :: forall a. Int -> Gen a -> Gen (List a)
+listOf :: forall m a. MonadRec m => Int -> GenT m a -> GenT m (List a)
 listOf = replicateMRec
 
 -- | Create a random generator which generates a vector of random values of a specified size.
-vectorOf :: forall a. Int -> Gen a -> Gen (Array a)
+vectorOf :: forall m a. MonadRec m => Int -> GenT m a -> GenT m (Array a)
 vectorOf k g = toUnfoldable <$> listOf k g
 
 -- | Create a random generator which selects a value from a non-empty array with
 -- | uniform probability.
-elements :: forall a. NonEmptyArray a -> Gen a
+elements :: forall m a. MonadRec m => NonEmptyArray a -> GenT m a
 elements xs = do
   n <- chooseInt zero (NEA.length xs - one)
   pure $ unsafePartial $ NEA.unsafeIndex xs n
 
 -- | Generate a random permutation of the given array
-shuffle :: forall a. Array a -> Gen (Array a)
+shuffle :: forall m a. MonadRec m => Array a -> GenT m (Array a)
 shuffle xs = do
   ns <- vectorOf (length xs) (chooseInt 0 top)
   pure (map snd (sortBy (comparing fst) (zip ns xs)))
 
--- | Run a random generator
-runGen :: forall a. Gen a -> GenState -> Tuple a GenState
-runGen = runState <<< unGen
-
--- | Run a random generator, keeping only the randomly-generated result
-evalGen :: forall a. Gen a -> GenState -> a
-evalGen = evalState <<< unGen
-
 -- | Sample a random generator
-sample :: forall a. Seed -> Size -> Gen a -> Array a
-sample seed sz g = evalGen (vectorOf sz g) { newSeed: seed, size: sz }
+sample :: forall m a. MonadRec m => Seed -> Size -> GenT m a -> m (Array a)
+sample seed sz g = evalGenT (vectorOf sz g) { newSeed: seed, size: sz }
 
 -- | Generate a single value using a randomly generated seed.
-randomSampleOne :: forall a. Gen a -> Effect a
+randomSampleOne :: forall m a. MonadEffect m => GenT m a -> m a
 randomSampleOne gen = do
-  seed <- randomSeed
-  pure $ evalGen gen { newSeed: seed, size: 10 }
+  seed <- liftEffect randomSeed
+  evalGenT gen { newSeed: seed, size: 10 }
 
 -- | Sample a random generator, using a randomly generated seed
-randomSample' :: forall a. Size -> Gen a -> Effect (Array a)
+randomSample' :: forall m a. MonadRec m => MonadEffect m => Size -> GenT m a -> m (Array a)
 randomSample' n g = do
-  seed <- randomSeed
-  pure $ sample seed n g
+  seed <- liftEffect randomSeed
+  sample seed n g
 
 -- | Get a random sample of 10 values. For a single value, use `randomSampleOne`.
-randomSample :: forall a. Gen a -> Effect (Array a)
+randomSample :: forall m a. MonadRec m => MonadEffect m => GenT m a -> m (Array a)
 randomSample = randomSample' 10
 
 -- | A random generator which simply outputs the current seed
-lcgStep :: Gen Int
-lcgStep = Gen $ state f where
+lcgStep :: forall m. Monad m => GenT m Int
+lcgStep = GenT $ state f where
   f s = Tuple (unSeed s.newSeed) (s { newSeed = lcgNext s.newSeed })
 
 -- | A random generator which approximates a uniform random variable on `[0, 1]`
-uniform :: Gen Number
+uniform :: forall m. Monad m => GenT m Number
 uniform = (\n -> toNumber n / toNumber lcgM) <$> lcgStep
 
 foreign import float32ToInt32 :: Number -> Int
-
--- | Perturb a random generator by modifying the current seed
-perturbGen :: forall a. Number -> Gen a -> Gen a
-perturbGen n gen = Gen do
-  void $ modify \s -> s { newSeed = lcgPerturb (float32ToInt32 n) s.newSeed }
-  unGen gen
